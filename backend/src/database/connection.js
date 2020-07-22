a633a721f29178d2892a759be397c7cbe517c1cb
Wagner Silva <wagnerjps@gmail.com>
2020-07-22 08:14:15 -0300

    update

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -6,9 +6,6 @@ const env = process.env.NODE_ENV;
 const config = (env === 'test') ? configuration.test : 
     (env === 'prod') ? configuration.production : configuration.development;
 
-console.log(config)
-//config = (env === 'test') ? configuration.development : configuration.development
-
 const connection = knex(config);
 
 module.exports = connection;
